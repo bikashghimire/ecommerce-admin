1d4e8fb7495d33d12a42e939566334ffa6a67fbd
bikashghimire <bikashghimire2610@gmail.com>
2024-12-29 19:28:43 +0200

    project setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="p-5">
+      <Button size="default" variant="destructive">
+        Click Me
+      </Button>
+    </div>
+  );
+}
